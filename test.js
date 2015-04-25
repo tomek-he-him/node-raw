a197612fc165abf62ee555e59c2a86ff502fd30e
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-04-25 22:50:14 +0200

    Specify loading raw text files

--- test.js
+++ test.js
@@ -1,9 +1,10 @@
 import test from 'tape-catch';
+import {readFileSync} from 'fs';
 
 import nodeRaw from './module/index';
 
 test('Doesn’t break the native `require`.', (is) => {
-  let isomorphicRequire = nodeRaw(require);
+  const isomorphicRequire = nodeRaw(require);
 
   is.equal(
     isomorphicRequire('./test/fixtures/itWorks'),
@@ -38,6 +39,20 @@ test('Doesn’t break the native `require`.', (is) => {
   is.end();
 });
 
-test.skip('Works with local files.');
+test('Loads raw text files.', (is) => {
+  const isomorphicRequire = nodeRaw(require);
 
-test.skip('Works with node modules.');
+  is.equal(
+    isomorphicRequire('raw!./test/fixtures/itWorks.txt'),
+    'It works with raw text files!\n',
+    'from local files'
+  );
+
+  is.equal(
+    isomorphicRequire('raw!babel/README.md'),
+    readFileSync('../node_modules/babel/README.md'),
+    'from module files'
+  );
+
+  is.end();
+});
