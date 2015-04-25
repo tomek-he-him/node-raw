f46d652af1a2c5542386ab604d94e663832ce853
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-04-25 21:42:47 +0200

    ✦

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var del = require('del');
+
+var gulp = require('gulp-help')(require('gulp'));
+var babel = require('gulp-babel');
+
+// `gulp build`
+// -----------------------------------------------------------------------------
+gulp.task('build',
+  'Compile everything.',
+  ['scripts']
+);
+
+// `gulp scripts`
+// -----------------------------------------------------------------------------
+var scripts = {
+  source: 'module/**/*.js',
+  target: '.'
+};
+
+gulp.task('scripts',
+  'Compile scripts to ES5.',
+  ['scripts:clean'],
+  function() {
+    return gulp.src(scripts.source)
+      .pipe(babel())
+      .pipe(gulp.dest(scripts.target))
+    ;
+  }
+);
+
+gulp.task('scripts:clean', false, function(done) {
+  del([
+    '!/module.js',
+    '!/gulpfile.js',
+    '!/test.js',
+    scripts.target + '/*.{js,js.map}'
+  ], done);
+});
+
+// `gulp clean`
+// -----------------------------------------------------------------------------
+gulp.task('clean',
+  'Remove all built files.',
+  ['scripts:clean']
+);
