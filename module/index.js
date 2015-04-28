d8f08698c5af4edbc717689a7bc3a941f6629b78
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-04-29 00:12:37 +0200

    Add setting `dirname`

--- module/index.js
+++ module/index.js
@@ -3,21 +3,23 @@
 const rawPrefix = /^raw!/;
 const root = new Function('return this')();
 
-export default (nativeRequire) => {
+export default (nativeRequire, settings) => {
+  const {dirname} = settings;
+
   // If in the browser, just return webpack’s `require`.
   if (root.window === root) return nativeRequire;
 
   // Else wrap `require` with the following function:
   else {
     const {readFileSync} = nativeRequire('fs');
+    const {resolve} = nativeRequire('path');
 
     return (moduleId) => (rawPrefix.test(moduleId) ?
 
       // If the `moduleId` starts with "raw!", return contents of the
       // succeeding file path.
       readFileSync(
-        moduleId
-          .replace(rawPrefix, ''),
+        resolve(dirname, moduleId.replace(rawPrefix, '')),
         {encoding: 'utf8'}
       ) :
 
